aec05887a9e655c2b3b4be02ec0ef2527c58f55e
Abhishek Kumar <abhivision3@gmail.com>
2021-04-17 12:40:37 +0530

    react UI

---
+++ block-BRaaav/code/hydro/src/components/App.js
@@ -0,0 +1,23 @@
+import Header from './Header';
+import Footer from './Footer';
+import Hero from './Hero';
+import Intro from './Intro';
+import Blog from './Blog';
+import Work from './Work';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Intro />
+      <Blog />
+      <Work />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
